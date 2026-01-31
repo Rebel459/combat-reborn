fc224c84640b534b06fc0fd3b53534585d7d7386
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-01-31 21:41:22 +1100

    Revert "pls fix"
    This reverts commit 98479b57eb2bb0772664fa59f32ab7f664e77211.

--- src/main/java/net/legacy/combat_reborn/tag/CRItemTags.java
+++ src/main/java/net/legacy/combat_reborn/tag/CRItemTags.java
@@ -13,8 +13,6 @@ public class CRItemTags {
 
     public static final TagKey<Item> RUBBLE_SHIELD = bindEnderscape("rubble_shields");
 
-    public static final TagKey<Item> TRIDENT_REPAIR_MATERIALS = bindLegaciesAndLegends("trident_repair_materials");
-
     public static final TagKey<Item> SOUP = bindCommon("foods/soup");
     public static final TagKey<Item> POTIONS = bindCommon("potions");
 
@@ -29,9 +27,5 @@ public class CRItemTags {
     @NotNull
     private static TagKey<Item> bindEnderscape(@NotNull String path) {
         return TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("enderscape", path));
-    }
-    @NotNull
-    private static TagKey<Item> bindLegaciesAndLegends(@NotNull String path) {
-        return TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("legacies_and_legends", path));
     }
 }
